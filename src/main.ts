41ede1e33f2e2141b3268a73810bd10f8ffbc015
Juno-Cheng <jonathan.cheng@argolabs.ai>
2024-10-05 12:50:28 -0700

    feat: default remove found d1 assignment

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,10 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing super duper cool game";
+const gameName = "My amazing game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Create a counter display
-let counter = 0;
-const counterDisplay = document.createElement("p");
-counterDisplay.innerHTML = `Counter: ${counter}`;
-app.append(counterDisplay);
-
-// Create the increment button
-const incrementButton = document.createElement("button");
-incrementButton.innerHTML = "Increment";
-incrementButton.addEventListener("click", () => {
-  counter++;
-  counterDisplay.innerHTML = `Counter: ${counter}`;
-});
-app.append(incrementButton);
-
-// Create the decrement button
-const decrementButton = document.createElement("button");
-decrementButton.innerHTML = "Decrement";
-decrementButton.addEventListener("click", () => {
-  counter--;
-  counterDisplay.innerHTML = `Counter: ${counter}`;
-});
-app.append(decrementButton);
