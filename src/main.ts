621560c333a8b45abf1c289286aa4e95c85ec17b
Juno-Cheng <jonathan.cheng@argolabs.ai>
2024-10-05 12:59:01 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,17 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// Create a counter display
+let counter = 0;
+const counterDisplay = document.createElement("p");
+counterDisplay.innerHTML = `Rocket Fuel: ${counter}`;
+app.append(counterDisplay);
 
 // Create the increment button
 const incrementButton = document.createElement("button");
 incrementButton.innerHTML = "&#128640;";
+incrementButton.addEventListener("click", () => {
+    counter++;
+    counterDisplay.innerHTML = `Rocket Fuel: ${counter}`;
+  });
 app.append(incrementButton);
-
