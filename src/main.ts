1151d5068bb97b905a8ba158a4db82eb91a4007a
Juno-Cheng <jonathan.cheng@argolabs.ai>
2024-10-05 14:36:16 -0700

    Step 4 Complete
    Remade Since I forgot to save

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,13 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-// Create a counter display
+// Create a counter display - Step 1
 let counter = 0;
 const counterDisplay = document.createElement("p");
 counterDisplay.innerHTML = `Rocket Fuel: ${counter}`;
 app.append(counterDisplay);
 
-// Create the increment button
+// Create the increment button - Step 2
 const incrementButton = document.createElement("button");
 incrementButton.innerHTML = "&#128640;";
 incrementButton.addEventListener("click", () => {
@@ -24,10 +24,31 @@ incrementButton.addEventListener("click", () => {
 });
 app.append(incrementButton);
 
-//SetInterval
+//SetInterval - Step 3
 const incrementCounter = () => {
   counter++;
   counterDisplay.innerHTML = `Rocket Fuel: ${counter}`;
 };
 
 setInterval(incrementCounter, 1);
+
+//Continuous Growth - Step 4
+let lastTimestamp = 0;
+
+function incrementCounterWithAnimationFrame(timestamp: number) {
+  if (lastTimestamp === 0) {
+    lastTimestamp = timestamp;
+  }
+
+  const deltaTime = timestamp - lastTimestamp;
+  lastTimestamp = timestamp;
+
+  counter +=  deltaTime / 1000;
+  counterDisplay.innerHTML = `Rocket Fuel: ${counter}`;
+
+  requestAnimationFrame(incrementCounterWithAnimationFrame);
+}
+
+// Start the animation frame loop
+requestAnimationFrame(incrementCounterWithAnimationFrame);
+ 
