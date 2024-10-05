480c96befb27bb100aa1cc86ceecf3b8486d4d71
Juno-Cheng <jonathan.cheng@argolabs.ai>
2024-10-05 12:54:44 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,9 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+
+// Create the increment button
+const incrementButton = document.createElement("button");
+incrementButton.innerHTML = "&#128640;";
+app.append(incrementButton);
+
